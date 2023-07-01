773ed9902a6a348fc0a352bb1e4c72b55f3ea1dd
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-07-01 23:22:20 +0300

    deployed

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,27 +1,28 @@
-
-import Head from 'next/head';
-import Image from 'next/image';
-import { useEffect } from 'react';
-import GradientBG from '../components/GradientBG.js';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect } from "react";
+import GradientBG from "../components/GradientBG.js";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   useEffect(() => {
     (async () => {
-      const { Mina, PublicKey } = await import('snarkyjs');
-      const { Add } = await import('../../../contracts/build/src/');
+      const { Mina, PublicKey } = await import("snarkyjs");
+      const { Add } = await import("../../../contracts/build/src/");
 
       // Update this to use the address (public key) for your zkApp account.
       // To try it out, you can try this address for an example "Add" smart contract that we've deployed to
       // Berkeley Testnet B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA.
-      const zkAppAddress = '';
+      const zkAppAddress =
+        "B62qpRsBFzYAQMeRs1QpwHwCrAazsQUAfjV142Rkio3GpQAppf1fgXx";
       // This should be removed once the zkAppAddress is updated.
       if (!zkAppAddress) {
         console.error(
           'The following error is caused because the zkAppAddress has an empty string as the public key. Update the zkAppAddress with the public key for your zkApp account, or try this address for an example "Add" smart contract that we deployed to Berkeley Testnet: B62qkwohsqTBPsvhYE8cPZSpzJMgoKn4i1LQRuBAtVXWpaT4dgH6WoA'
         );
       }
-      //const zkApp = new Add(PublicKey.fromBase58(zkAppAddress))
+      const zkApp = new Add(PublicKey.fromBase58(zkAppAddress));
+      console.log("Addr,", zkApp.address.toBase58());
     })();
   }, []);
 
