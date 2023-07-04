cfe028d51c39535d1578ca26a7a6ec9ab7a81e58
Lauri Peltonen <20242241+microbecode@users.noreply.github.com>
2023-07-04 16:28:30 +0300

    add contact

--- README.md
+++ README.md
@@ -22,6 +22,16 @@ This project aimed to build a website and a circuit, and connect them together v
 
 Unfortunately, following various Mina tutorials and examples, I was unable to get the UI to work with a wallet. Different examples use different versions of things, and all example code seems to be outdated. Furthermore, I was unable to find a working React UI From the [examples provided](https://zkappsformina.com/).
 
+I couldn't get anything working from the intended background workers. But, on the other hand, I couldn't much working without them either. The wallet loads correctly, but after that things just get stuck.
+
+Some of the functionality is missing in the UI.
+
 The circuit and its unit tests somewhat work, but it's also possible that the circuit doesn't work exactly as intended. The circuit was also successfully deployed at Berkeley testnet, at address B62qpHJ1XKAJFuubmiMXeTzSGGViDnQtmJ8cvJMRWh6Z2VAiRQfGPZK .
 
 In any case, I learned a lot of Mina while doing this, which was also my original target.
+
+## Licensing and blah blah
+
+MIT for everything.
+
+Wanna get in touch? Poke me in Telegram (username Lauri_P) or [Twitter](https://twitter.com/LauriPelto).
--- contracts/src/Propose.ts
+++ contracts/src/Propose.ts
@@ -1,3 +1,5 @@
+// LICENSE: MIT
+
 import {
   Field,
   SmartContract,
@@ -11,42 +13,14 @@ import {
   Bool,
 } from 'snarkyjs';
 
-/**
- * Basic Example
- * See https://docs.minaprotocol.com/zkapps for more info.
- *
- * The Add contract initializes the state variable 'num' to be a Field(1) value by default when deployed.
- * When the 'update' method is called, the Add contract adds Field(2) to its 'num' contract state.
- *
- * This file is safe to delete and replace with your own contract.
- */
-
 export class Propose extends SmartContract {
-  //@state(Field) num = State<Field>();
-  //@state(MyArray3) targets = State<MyArray3>();
-
-  /*   @state(PublicKey) user1 = State<PublicKey>();
-  @state(PublicKey) user2 = State<PublicKey>();
-  @state(PublicKey) user3 = State<PublicKey>(); */
   @state(PublicKey) proposer = State<PublicKey>();
   @state(PublicKey) proposerChoice = State<PublicKey>();
-  //@state(PublicKey) chosen = State<PublicKey>();
   @state(Bool) matched = State<Bool>();
 
   init() {
     super.init();
-    //this.num.set(Field(1));
-    // let user1 = users['Bob'].toPublicKey();
-    // let user2 = users['SuperBob'].toPublicKey();
-    // let user3 = users['MegaBob'].toPublicKey();
-    // let user4 = users['Jack'].toPublicKey();
 
-    // let arr: MyArray3 = { value: [user1, user2, user3] };
-    // this.targets.set(arr);
-    // this.proposer.set(user4);
-    /*     this.user1.set();
-    this.user2.set(users['SuperBob'].toPublicKey());
-    this.user3.set(users['MegaBob'].toPublicKey()); */
     this.matched.set(Bool(false));
   }
 
